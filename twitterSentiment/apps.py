717be461bd0ee507f287c41456587488c3c57433
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2016-10-23 09:18:56 +0545

    Initial commit with models

---
+++ twitterSentiment/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TwittersentimentConfig(AppConfig):
+    name = 'twitterSentiment'
---
+++ twitterSentiment/models.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class StoreTrainingFile(models.Model):
+    fname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.fname
+
+class StoreTestingFile(models.Model):
+    fname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.fname
+
+class TweetCollection(models.Model):
+    tweet_text = models.TextField()
+    tweet_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.tweet_text
+
+class TestingResult(models.Model):
+    no_of_train_data = models.IntegerField()
+    no_of_test_data = models.IntegerField()
+    no_of_pos_classified = models.IntegerField()
+    no_of_neg_classified = models.IntegerField()
+    no_of_actual_pos = models.IntegerField()
+    no_of_actual_neg = models.IntegerField()
+
+class ValidationResult(models.Model):
+    train_split_ratio = models.FloatField()
+    test_split_ratio = models.FloatField()
+    no_of_pos_train_data = models.IntegerField()
+    no_of_neg_train_data = models.IntegerField()
+    no_of_pos_test_data = models.IntegerField()
+    no_of_neg_test_data = models.IntegerField()
+    no_of_pos_classified = models.IntegerField()
+    no_of_neg_classified = models.IntegerField()
+    no_of_actual_pos = models.IntegerField()
+    no_of_actual_neg = models.IntegerField()
+    precision = models.FloatField()
+    recall = models.FloatField()
+    f_measure = models.FloatField()
+    accuracy = models.FloatField()
